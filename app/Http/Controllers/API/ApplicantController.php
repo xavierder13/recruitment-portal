b0c4f4988eb790697915c6bef4e35636d74f63b5
Xavier De Guzman <47979972+xavierder13@users.noreply.github.com>
2024-04-25 16:40:14 +0800

    Update ApplicantController.php

--- app/Http/Controllers/API/ApplicantController.php
+++ app/Http/Controllers/API/ApplicantController.php
@@ -545,15 +545,15 @@ class ApplicantController extends Controller
 									<span>Human Resource Team</span>
 								</html>';
 	
-				if($email_name)
-				{
-					Mail::send([], [], function ($message) use ($html, $email_name) {
-						$message->to($email_name)
-										->subject('Addessa Online Job Application')
-										->from('addessa.corporation2013@gmail.com', 'Addessa Corporation')
-										->setBody($html, 'text/html');
-					});
-				}
+				// if($email_name)
+				// {
+				// 	Mail::send([], [], function ($message) use ($html, $email_name) {
+				// 		$message->to($email_name)
+				// 						->subject('Addessa Online Job Application')
+				// 						->from('addessa.corporation2013@gmail.com', 'Addessa Corporation')
+				// 						->setBody($html, 'text/html');
+				// 	});
+				// }
    
 				// Mail::send([], [], function($message) {
 				// 	$message->to('jeremiahherreria19@gmail.com', 'Tutorials Point');
